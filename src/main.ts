3c85be5e1be83d7d73131f85a113142643d49c4d
Dranee <adrianaviladiaz93@gmail.com>
2024-03-20 13:07:19 -0500

    update cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3333;
   const CORS = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://open--market.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   };
   app.enableCors(CORS);
