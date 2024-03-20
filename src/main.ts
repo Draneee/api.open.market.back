eb65118eaf4a78ce39483a2412ff160ce1efeb09
Dranee <adrianaviladiaz93@gmail.com>
2024-03-20 10:41:13 -0500

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3333;
   const CORS = {
     origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   };
   app.enableCors(CORS);
-  await app.listen(3333);
+  await app.listen(port);
 }
 bootstrap();
